0238858a3a93909ba65f95ab62ea4810d7ad45c7
BluDemons <jar.salazar@yavirac.edu.ec>
2019-07-11 23:08:28 -0500

    tamaño letra

--- src/app/contactos/contactos.component.ts
+++ src/app/contactos/contactos.component.ts
@@ -28,10 +28,22 @@ export class ContactosComponent implements OnInit {
       name: ['',[Validators.required, Validators.minLength(6)]],
       lastname: ['',[Validators.required, Validators.minLength(6)]],
       email: ['', [Validators.required, Validators.pattern('^[a-z0-9_.+-]+@[a-zA-Z0-9-]+.[a-z]+$')]],
-      password: ['' ,[Validators.required,Validators.minLength(6)]],
+      password: ['' ,[Validators.required,Validators.minLength(6)]],//this.validatePassword],
       mensaje:['',[Validators.required]]
     });
   }
+
+  // private validatePassword(control: AbstractControl) {
+  //   const password = control.value;
+  //   let error = null;
+  //   if (!password.includes('$')) {
+  //     error = { ...error, dollar: 'needs a dollar symbol' };
+  //   }
+  //   if (!parseFloat(password[0])) {
+  //     error = { ...error, number: 'must start with a number' };
+  //   }
+  //   return error;
+  // }
 
   validaLoginForm(){
     if(this.loginform.valid){
@@ -48,7 +60,17 @@ export class ContactosComponent implements OnInit {
     }
     if(this.loginform.invalid){
       console.log(this.loginform.controls['email'].errors.pattern)
-      alert(JSON.stringify(this.loginform.controls['email'].errors.pattern))
+      alert('Por favor llene los campos con *')
     }
   }
+
+  public getError(controlName: string): string {
+    let error = '';
+    const control = this.loginform.get(controlName);
+    if (control.touched && control.errors != null) {
+      error = JSON.stringify(control.errors);
+    }
+    return error;
+  }
+
 }
