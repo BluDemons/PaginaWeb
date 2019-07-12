c35c1f5c1ce6fe43c1020a7a74ed905e4b57871c
BluDemons <jar.salazar@yavirac.edu.ec>
2019-07-11 22:40:12 -0500

    Formulario

--- src/app/contactos/contactos.component.ts
+++ src/app/contactos/contactos.component.ts
@@ -10,7 +10,8 @@ export class ContactosComponent implements OnInit {
 
   loginform: FormGroup
   registroUsuariosForm: FormGroup
-  usuario:string
+  name:string
+  lastname: string
   email: string
   password: string
   mensaje :any
@@ -24,8 +25,9 @@ export class ContactosComponent implements OnInit {
 
   crearLoginForm(){
     this.loginform = this.formBuilder.group({
-      usuario: ['', Validators.required, Validators.minLength(6)],
-      email: ['', [Validators.required, Validators.email]],
+      name: ['',[Validators.required, Validators.minLength(6)]],
+      lastname: ['',[Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9_.+-]+@[a-zA-Z0-9-]+.[a-z]+$')]],
       password: ['' ,[Validators.required,Validators.minLength(6)]],
       mensaje:['',[Validators.required]]
     });
@@ -33,17 +35,20 @@ export class ContactosComponent implements OnInit {
 
   validaLoginForm(){
     if(this.loginform.valid){
-      this.usuario=this.loginform.controls['usuario'].value
+      this.name=this.loginform.controls['name'].value
+      this.lastname=this.loginform.controls['lastname'].value
       this.email = this.loginform.controls['email'].value
       this.password = this.loginform.controls['password'].value
+      console.log(this.loginform)
     }else{
-      this.usuario = ' '
-      this.email = ''
-      this.password = ''
-      this.mensaje =  JSON.stringify(this.loginform.controls['usuario'].errors)
-      if(this.mensaje){
-        alert('dawdwad')
-      }
+      this.name = JSON.stringify(console.log(this.loginform.controls['name'].errors))
+      this.lastname = JSON.stringify(console.log(this.loginform.controls['lastname'].errors))
+      this.email = JSON.stringify(console.log(this.loginform.controls['email'].errors))
+      this.password = JSON.stringify(console.log(this.loginform.controls['password'].errors))
+    }
+    if(this.loginform.invalid){
+      console.log(this.loginform.controls['email'].errors.pattern)
+      alert(JSON.stringify(this.loginform.controls['email'].errors.pattern))
     }
   }
 }
