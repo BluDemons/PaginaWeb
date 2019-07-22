11acc608950884f2de7b325f103ed112e44a7adc
Your Name <you@example.com>
2019-07-22 09:03:49 -0500

    pdfs

--- src/app/contactos/contactos.component.ts
+++ src/app/contactos/contactos.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+//generar reportes
+import jsPDF from 'jsPDF';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-contactos',
   templateUrl: './contactos.component.html',
   styleUrls: ['./contactos.component.scss'],
-  template:` 
-  <h2>{{"helo"+ nombre}}</h2>
-  <button (click)="fireEvent()">Sent event</button>
-  `,
 })
 export class ContactosComponent implements OnInit {
 
@@ -33,11 +32,52 @@ export class ContactosComponent implements OnInit {
   ngOnInit() {
     this.crearLoginForm();
     this.operadoras=['Claro','CNT','Movistar','Twenty']
-    
   }
+
   fireEvent(){
     this.childEvent.emit('Hey juan Andres');
   }
+  
+  pdf(){
+    let x = 25
+    let y = 25
+    const sizemm = 0.3515
+    let size = 14
+    let intelineado = 10
+    let rosa= '#ff0080'
+    let texto='Coordinar (en unidades declaradas al inicio del documento PDF) contra el borde superior de la página.'
+    let doc=new jsPDF({
+      orientation:'L',
+      unit:'mm',
+      format: 'A4',
+      compress:true,
+    })
+    // doc.setFontSize(size)
+    // doc.text('MI PDF con tamaño de letra',x,y) 
+    // y=y+(size*sizemm)+intelineado
+    // doc.text('tengo una nueva linea',x,y)
+    // size=22
+    // y=y+(size*sizemm)+intelineado
+    // doc.text('cambie el tamaño de letra',x,y)
+    // doc.setTextColor(rosa)
+    doc.setFontSize(size)
+    x=x+(size*sizemm)+intelineado
+    doc.text('DATOS GENERALES',x,y)
+    y=y+(size*sizemm)+intelineado
+    doc.text('Nombre:',x,y)
+    x=y+(size*sizemm)+intelineado
+    doc.text('Apellido:',x,y)
+    y=y+(size*sizemm)+intelineado
+    doc.text('Dirección:',x,y)
+    y=y+(size*sizemm)+intelineado
+    doc.text('Teléfono:',x,y)
+    x=y+(size*sizemm)+intelineado
+    doc.text('Fecha de Nacimiento:',x,y)
+    y=y+(size*sizemm)+intelineado
+    doc.text(texto,x,y,{maxWidth:200})
+    doc.save('juanPDF')
+  }
+
   crearLoginForm(){
     this.loginform = this.formBuilder.group({
       tipoSa:this.addTiposSa(),
